7c97a2d961f9c174fa3a033d8ce2a5cf51e46afb
DraghiciLucian Rafael <183347@AIV.EDMC.STU>
2016-12-05 14:29:13 -0800

    Initial Commit

---
+++ GameJam proj/Assets/GolemControls1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GolemControls1 : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKey("A"))
+        {
+            this.transform.position += new Vector3 ()
+        }
+	}
+}
